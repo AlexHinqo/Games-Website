5b92b84b9c35b20201a0b4e50f5c526fa47a0e27
Alex Tinoco <tinocoalex003@gmail.com>
2024-04-02 12:53:57 +0200

    feat : add answer input

--- view/connection.php
+++ view/connection.php
@@ -8,4 +8,12 @@
         <p class="text">Quel écrivain français du XIXe siècle a été influent pour son œuvre "Les Misérables" ?</p>
     </div>
 </div>
+
+<div class="qanswer">
+    <input type="text" placeholder="Votre réponse...">
+    <button aria-label="Valider la réponse" class="icon-button" id="bt-account">
+      <img src="public/assets/icon-account.png" alt="">
+    </button>
+</div>
+
 </body>
